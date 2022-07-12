91463fcc25d57980ebb618e4d080399bafe239bf
문승규 <munseung-gyu@munseung-gyuui-MacBookAir.local>
2022-07-12 20:28:16 +0900

    Add search animation and scroll animation

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,13 +1,12 @@
 import styled from "styled-components"
 
 
-const Re = styled.div`
-    color:red;
-`
 function Home(){
     return(
-        <div style={{backgroundColor:'black',height:'200vh',width:'100vw'}}>
+        <div style={{backgroundColor:'whitesmoke',height:'200vh',width:'100vw'}}>
+            
         </div>
         )
 }
+
 export default Home

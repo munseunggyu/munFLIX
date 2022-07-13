07a60dc0f0dbac6b328d8588b028a5fbd4da0cef
문승규 <munseung-gyu@munseung-gyuui-MacBookAir.local>
2022-07-13 16:59:24 +0900

    Add main banner

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query"
 import styled from "styled-components"
 import { getMovies, IGetMoviesResult } from "../api"
+import { makeImagePath } from "../utilts"
 
 const Wrapper = styled.div`
     background:black;
@@ -11,13 +12,37 @@ const Lodder = styled.div`
     justify-content:center;
     align-items:center;
 `
+const Banner = styled.div<{bgPhoto:string}>`
+    height:100vh;
+    display: flex;
+    flex-direction:column;
+    justify-content:center;
+    padding:60px;
+    background-image: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,1)),
+    url(${props=>props.bgPhoto});
+    background-size:cover;
+`
+const Title = styled.h2`
+    font-size:60px;
+    margin-bottom:20px;
+`
+const Overview = styled.p`
+    font-size:36px;
+    width:50%;
+`
 
 function Home(){
     const {data,isLoading} = useQuery<IGetMoviesResult>(['movies','nowPlaying'],getMovies)
     return(
         <Wrapper>
             {
-                isLoading ? <Lodder> Lodding... </Lodder> : null
+                isLoading ? <Lodder> Lodding... </Lodder> : 
+                <>
+                    <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || '')}>
+                        <Title> {data?.results[0].title}</Title>
+                        <Overview > {data?.results[0].overview} </Overview>
+                    </Banner>
+                </>
             }
         </Wrapper>
         )
